385f3a6f45015569d2994db70d24fb5bf50a884a
thibmaek <thibault.maekelbergh@icloud.com>
2021-11-16 21:38:15 +0100

    💥 Deprecate support for InfluxDB v1.8

--- config.go
+++ config.go
@@ -21,46 +21,6 @@ type Config struct {
 	MonitoredReleases []interface{}
 }
 
-func getInfluxDBConfig(c *toml.Tree) (h string, t string, b Database) {
-	influxCfg := c.Get("influxdb").(*toml.Tree).ToMap()
-
-	var influxToken string
-	var influxBucket Database
-
-	host := fmt.Sprintf("%s:%d", influxCfg["host"].(string), influxCfg["port"].(int64))
-
-	token, tokenExists := influxCfg["token"]
-	if tokenExists {
-		// InfluxDB 2 requires org + bucket
-		influxToken = token.(string)
-	} else {
-		// InfluxDB 1.8 token is represented by user + pass joined with ':'
-		influxToken = fmt.Sprintf("%s:%s", influxCfg["user"].(string), influxCfg["password"].(string))
-	}
-
-	bucket, bucketExists := influxCfg["bucket"]
-	org, orgExists := influxCfg["org"]
-	if bucketExists && orgExists {
-		// InfluxDB 2 uses org + bucket combination
-		influxBucket = Database{
-			Org:    org.(string),
-			Bucket: bucket.(string),
-		}
-	} else {
-		// InfluxDB 1.8 uses database + retention policy as the bucket and empty org name
-		rp, rpExists := influxCfg["retention_policy"]
-		if !rpExists {
-			rp = ""
-		}
-		influxBucket = Database{
-			Org:    "",
-			Bucket: fmt.Sprintf("%s/%s", influxCfg["database"].(string), rp.(string)),
-		}
-	}
-
-	return host, influxToken, influxBucket
-}
-
 func GetConfig(cfgPath string) *Config {
 	f, err := ioutil.ReadFile(cfgPath)
 	if err != nil {
@@ -72,14 +32,17 @@ func GetConfig(cfgPath string) *Config {
 		log.Fatalf("Failed to read config file: %v", err)
 	}
 
-	influxHost, influxToken, influxBucket := getInfluxDBConfig(config)
+	influxCfg := config.Get("influxdb").(*toml.Tree).ToMap()
 
 	return &Config{
 		DiscogsToken: config.Get("discogs.token").(string),
 
-		InfluxHost:   influxHost,
-		InfluxToken:  influxToken,
-		InfluxBucket: influxBucket,
+		InfluxHost:  fmt.Sprintf("%s:%d", influxCfg["host"].(string), influxCfg["port"].(int64)),
+		InfluxToken: influxCfg["token"].(string),
+		InfluxBucket: Database{
+			Org:    influxCfg["org"].(string),
+			Bucket: influxCfg["bucket"].(string),
+		},
 
 		MonitoredReleases: config.Get("releases.monitored").([]interface{}),
 	}
